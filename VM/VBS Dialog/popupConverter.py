7b058f54e94e140a21eae0f51dcd324b182805ad
Dan Gorman <danthemangorman@gmail.com>
2017-12-12 22:44:37 -0500

    organized

--- VM/VBS Dialog/popupConverter.py
+++ VM/VBS Dialog/popupConverter.py
@@ -22,7 +22,7 @@ for piece in pieces:
 	for subPiece in subPieces:
 		output+='result = MsgBox("'+ subPiece[2].strip('"') +'", vbOk+vbExclamation, "'+ subPiece[1].strip('"') +'")\n\n'
 
-	filename = './' + subPiece[4].replace(' ','') + (('/'+subPiece[5].replace(' ','')) if subPiece[5]!='x' else '') + '/' +str(piece)+'.vbs'
+	filename = './popups/' + subPiece[4].replace(' ','') + (('/'+subPiece[5].replace(' ','')) if subPiece[5]!='x' else '') + '/' +str(piece)+'.vbs'
 	os.makedirs(os.path.dirname(filename), exist_ok=True)
 	with open(filename, 'w+') as f:
 		f.write(output)
